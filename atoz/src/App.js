082efd909eef42165181f81cc90b80d5a45c6850
georgyantonov <100569135+georgyantonov@users.noreply.github.com>
2022-09-21 23:59:44 +0300

    24:00

--- atoz/src/App.js
+++ atoz/src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
-
+import React from 'react';
+import { useState } from 'react';
 function App() {
+
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('Your comment');
+
+  function inc(){
+    setLikes(likes+1);
+    console.log(likes);
+  }
+
+  function dec(){
+    setLikes(likes-1);
+    console.log(likes);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <button onClick={inc}>Инкремент</button>
+      <button onClick={dec}>Декремент</button>
+      <input 
+      type = "text" 
+      name = "comment" 
+      value = {value} 
+      onChange = { (e) => setValue(e.target.value)}
+      onFocus = { (e) => e.target.value = null}
+      />
     </div>
   );
 }
