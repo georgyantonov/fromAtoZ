78b1a3478e31cf41281d42a5bc076278a4e0a348
Georgy Antonov <georgy.antonov@dalee.ru>
2022-10-09 18:20:26 +0300

    promo

--- atoz/src/Components/Post.jsx
+++ atoz/src/Components/Post.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import '../styles/post.css'
+import React, { useState } from 'react'
+import '../styles/post.scss'
 import MyButton from './UI/button/MyButton'
 import {useNavigate} from 'react-router-dom'
+import { useEffect } from 'react'
 
 export default function Post(props) {
   const router = useNavigate()
+  const [promo, setPromo] = useState('posts posts__post')
+  useEffect(() =>{
+    if(props.post.id % 10 == 0){
+      setPromo('posts posts__post posts__post_promo')
+    }
+  })
   return (
-    <div className='post' id={props.post.id}>
+    <div className='posts' id={props.post.id}>
+      <div className={promo}>
         <div className="post__content">
             <strong>{props.post.id}. {props.post.title}</strong>
             <div>{props.post.body}</div>
@@ -15,6 +23,7 @@ export default function Post(props) {
             <MyButton onClick={() => router(`/posts/${props.post.id}`)}>Открыть</MyButton>
             <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
             </div>
+      </div>
     </div>
   )
 }
